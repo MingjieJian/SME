b550b6cf466cd360059f51bdcd37d08dcaa80844
Mingjie Jian <jian-mingjie@outlook.com>
2025-07-22 14:12:58 +0200

    mosify smelib/etup.py to fit Mac compilation

--- src/pysme/smelib/setup.py
+++ src/pysme/smelib/setup.py
@@ -2,13 +2,19 @@
 from distutils.core import Extension, setup
 from os.path import abspath, dirname, join
 
-import numpy
+import numpy, sys
 from libtools import get_full_libfile
 
-libdir = get_full_libfile()
-libdir = abspath(dirname(libdir))
-include_dirs = [numpy.get_include()]
-include_dirs += [libdir]
+libdir = abspath(dirname(get_full_libfile()))
+include_dirs = [numpy.get_include(), libdir]
+
+extra_link_args = []
+if sys.platform == "darwin":
+    # Ensure the extension first searches a sibling ``lib/`` directory at runtime
+    # for libsme.{dylib,so}.  @loader_path expands to the directory containing
+    # the *_smelib*.so itself, so placing libs in ``pysme/smelib/lib`` works both
+    # in editable installs and built wheels.
+    extra_link_args.append("-Wl,-rpath,@loader_path/lib")
 
 module = Extension(
     "_smelib",
@@ -17,6 +23,7 @@ module = Extension(
     include_dirs=include_dirs,
     libraries=["sme"],
     library_dirs=[libdir],
+    extra_link_args=extra_link_args
 )
 
 setup(ext_modules=[module])
